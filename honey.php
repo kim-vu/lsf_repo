315fe57905714c37ec803e186ed3193885b5ca8f
Kim Vu <kim@readrogue.com>
2020-05-30 00:58:12 -0700

    Committing website files for the first time

---
+++ honey.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="styles/master-style.css"/>
+    <link rel="stylesheet" href="https://use.typekit.net/lde3etj.css">
+    <title>Honey</title>
+    <link rel="icon" href="icons/lonestar%20specialty%20foods%20favicon.png" type="image/gif" sizes="16x16">
+</head>
+
+<body>
+    
+    <!-- PAGE HEADER AND NAV BAR -->
+    <div class="header">
+    <nav class="nav-1 col-4" role="navigation">
+        <ul>
+            <li><a class="nav-1" href="/honey">Honey</a></li>
+            <li><a class="nav-1" href="/markets">Markets</a></li>
+            <li><a class="nav-1" href="/recipes">Recipes</a></li>
+            <li><a class="nav-1"href="/about">About</a></li>
+        </ul>
+    </nav>
+    
+    <!-- PAGE TITLE & LOGO -->
+    <div class="col-4"><a href="/"><img src="icons/lonestar%20specialty%20foods%20logo.png" alt="Lonestar Specialty Foods" id="lsf-logo"></a></div>
+    
+    <!-- SECONDARY NAV (SOCIAL AND CART) -->
+    <nav class="nav-2 col-4" role="navigation">
+        <ul>
+            <li><a href="facebook.com/lonestarspecialtyfoods"><img src="icons/LSF%20FB%20Logo%20Black%20100.png" alt="Lonestar Specialty Foods Facebook Page" width="15%"></a></li>
+            <li><a href="instagram.com/lonestarspecialtyfoods"><img src="icons/LSF%20IG%20Logo%20Black.png" alt="Lonestar Specialty Foods Instagram Page" width="15%"></a></li>
+            <li><a href="cart.html">Cart</a></li>
+        </ul>
+    </nav>
+    </div>
+    <!-- PRODUCTS -->
+    
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="styles/master-style.css"/>
+    <link rel="stylesheet" href="https://use.typekit.net/lde3etj.css">
+    <title>Lonestar Specialty Foods</title>
+    <link rel="icon" href="icons/lonestar%20specialty%20foods%20favicon.png" type="image/gif" sizes="16x16">
+</head>
+
+<body>
+    <!-- PAGE HEADER AND NAV BAR -->
+    <div class="header">
+    <nav class="col-4" role="navigation">
+        <ul id="nav-bar">
+            <li><a class="nav-1" href="/honey">Honey</a></li>
+            <li><a class="nav-1" href="/markets">Markets</a></li>
+            <li><a class="nav-1" href="/recipes">Recipes</a></li>
+            <li><a class="nav-1"href="/about">About</a></li>
+        </ul>
+    </nav>
+    
+    <!-- PAGE TITLE & LOGO -->
+    <div class="col-4"><a href="/"><img src="icons/lonestar%20specialty%20foods%20logo.png" alt="Lonestar Specialty Foods" id="lsf-logo"></a></div>
+    
+    <!-- SECONDARY NAV (SOCIAL AND CART) -->
+    <nav class="nav-2 col-4" role="navigation">
+        <a href="facebook.com/lonestarspecialtyfoods"><img src="icons/LSF%20FB%20Logo%20Black%20100.png" alt="Lonestar Specialty Foods Facebook Page" width="15%"></a>
+        <a href="instagram.com/lonestarspecialtyfoods"><img src="icons/LSF%20IG%20Logo%20Black.png" alt="Lonestar Specialty Foods Instagram Page" width="15%"></a>
+        <a href="cart.html">Cart</a>
+    </nav>
+    </div>
+    <!-- IMAGE CAROUSEL -->
+    <main role="main" class="carousel row">
+        <div class="slides fade">
+            <img src=images/angel%20feeds%20bees.jpg alt="beekeepers in dallas" style="width:100%">
+            <div class="text">Subduing the bees with smoke that interferes with the bees' sense of smell, so they cannot detect low concentrations of pheromones and do not receive the signal to swarm.</div>
+        </div>
+        <div class="slides fade">
+            <img src=images/prying%20frame%20out.jpg alt="beekeepers in dallas" style="width:100%">
+            <div class="text">We carefully remove frames from the hive using a frame tool to inspect the bees and assess the health of the population.</div>
+        </div>
+        <div class="slides fade">
+            <img src=images/removing%20frame.jpg alt="local texas honey" style="width:100%">
+            <div class="text">We carefully remove frames from the hive using a frame tool to inspect the bees and assess the health of the population.</div>
+        </div>
+        <div class="slides fade">
+            <img src=images/inspecting%20frame.jpg alt="local texas honey" style="width:100%">
+            <div class="text">We carefully remove frames from the hive using a frame tool to inspect the bees and assess the health of the population.</div>
+        </div>
+        <div class="slides fade">
+            <img src=images/id%20queen.jpg alt="beekeepers in dallas" style="width:100%">
+            <div class="text">Every healthy hive has one queen, and our beekeepers look through each one to identify her and check for the presence of larvae.</div>
+        </div>
+        <div class="slides fade">
+            <img src=images/id%20queen%20pen.jpg alt="dallas apiary" style="width:100%">
+            <div class="text">Every healthy hive has one queen, and our beekeepers look through each one to identify her and check for the presence of larvae.</div>
+        </div>
+        
+        <a class="prev" onclick="plusSlides(-1)">&lt;</a>
+        <a class="next" onclick="plusSlides(1)">&gt;</a>
+        
+        <div style="text-align: center;">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+            <span class="dot" onclick="currentSlide(4)"></span>
+            <span class="dot" onclick="currentSlide(5)"></span>
+            <span class="dot" onclick="currentSlide(6)"></span>
+        </div>
+    </main>
+    <!-- FEATURED ITEMS -->
+    <!-- NARRATIVE -->
+    <article class="col-12 row">
+        <section class="col-3"><h2><a href="about.html">The Beekeepers</a></h2>
+            <p>William and Marcia Branstetter have been in the business of bees since 2007, and lovingly care for all the Lonestar Specialty Foods beehives located across Collin and Denton County. Lonestar Specialty Foods is headquarted in Aubrey, Texas and is dedicated to providing fresh, wholesome, sustainably sourced local honey to the Dallas-Fort Worth area.</p>
+        </section>
+        <section><h2>The Bees</h2>
+            <p>We strive to keep the strongest and healthiest hives, each one maintaining a population of about 50,000 bees, ruled by a single queen and a few hundred male drones. Our beekeepers record regular observations on the hives to properly time feeding and harvesting schedules. Each honey bee will visit 50-100 flowers during a single collection flight from the hive, and the entire colony will produce 60-100 pounds of honey in one year. We make sure to keep our bees on private, not agricultural land, to prevent picking up pesticides. It should be noted that bees act as a biological filter, as they die when coming into contact with toxins, and so never bring back pollutants to the hive.</p>
+        </section>
+        <section><h2>The Honey</h2>
+            <p>There is absolutely nothing in this world like fresh, raw honey. That's why every bible will tell you the promised land flows with milk and honey. It's been an undisputed fact of life.</p>
+            <h3>Raw</h3>
+            <p>Meaning untreated &mdash; unheated, unfiltered, straight&#45;from&#45;the&#45;hive pure honey. We extract our honey using a centrifuge and then strain it through a stainless steel sieve. Supermarket honey is often pasteurized and filtered in order to increase its shelf life. By denaturing the enzymes and removing the pollen, the honey stabilizes and thus crystallizes at a slower rate. However, this process destroys the very two aspects of honey that make it beneficial to your health and reduces it to flavored sugar.</p>
+            <h4>Enzymes</h4>
+            <p>Have you ever wondered how the bees make honey? It's pretty miraculous. The honeybees fly around our wildflower fields picking up nectar, the sugary fluid produced by flower glands in order to attract pollinators. They store it in their extra stomach, or &quot;crop&quot; where it gets mixed with enzymes that transform the nectar's chemical composition and pH, creating a substance suitable for long-term storage. And it doesn't just go through one honeybee &mdash; it is a whole hive effort. When the bee returns to the hive, it regurgitates the liquid and passes it onto another bee, allowing it to be digested and processed by each individual's enzymes before finally getting deposited into a honeycomb. The term &#39;regurgitation&#39; is generally unappetizing to humans, and is avoided in the large&#45;scale commercial branding of this bee product&#59; however, try to think about this outside the scale of the human body and on the scale of life at large and the communities within it, because this is a truly spectacular and romantic process. These stomachs are unlike the ones we use to <a href="recipes.html">digest</a> our food. These are little factories full of proteins that act as catalysts to chemical reactions. They trigger change, and not just any kind of garden&#39;variety change. They are working on transforming what are essentially flower pheromones into a substance that never dies. They take the most common, vulgar aspect of life &mdash; its primative call to reproduce for reproduction's sake &mdash; and they ingest it, digest it, enrich it, and furthermore, share it &mdash; so that it can be refined through the numerous and unique bodies of the hive. Then they dehydrate it by fanning the honeycomb with their wings and seal the comb with a secretion that becomes beeswax. It is the perfect process of preservation. They make and store it to feed the hive through the cold winter months, but actually do such an exceptional job that it can be stored indefinitely. Humans have found honey within Egyptian tombs that was able to be reconstituted for consumption, thousands of years after its production. Honey is the exemplary product of life everlasting.</p>
+            <p>And so thus, it must be good for you. The enzymes in raw honey, like diastase, invertase, catalase, glucose oxidase, and acid phosphatase all aid the body with digestion by triggering chemical reactions that break down complex molecules into the major macromolecules your body needs to build muscle and burn for energy. If you think about it carefully, there is evidently nothing more important for your health than your ability to digest food. A happy gut is a happy human, and honey does its good part in helping you move your fuel from point A to point B.</p>
+            <h4>Pollen</h4>
+            <p>Do you suffer from seasonal allergies? Some call it hay fever. Whatever the name, the cause is an overactive immune system, releasing excessive histamines to fight off foreign invaders to the body &mdash; in this case, the foreigners being pollen. Texas, in particular, is a breeding ground for allergies, no matter how long you've lived in the Lone Star State. Because of its vast surface area and unpredictable extreme weather, Texas grows extremely diverse wildflowers. Nature weeds out the weak, and the strong don't look like any one thing. They come in all sorts of shapes, sizes, and so too in all sorts of pollen varieties, which vary again from year to year. Consuming local honey that still contains traces of pollen allows the body to recognize the substance as part of the self rather than as an outside threat. It is a natural and effective way to integrate your body with the environment in which it lives, so that it can freely expose itself without wasting energy on self-attack. Many sources on the internet suggest consuming honey from within a 15 mile radius of home. However, those sources sure aren't talking about Texas. A summer storm in Texas will blow up wind from much further than 15 miles, and will scatter wildflowers from all over the state. Not to mention, flowers grow overnight here, given the extreme volume of water they'll receive followed by extreme heat and sunshine. Change comes in fast and full of drama, and you'll have to run to keep up. This is why we carry a variety of <a href="honey.html">honey</a> at Lonestar Specialty Foods, ranging from single sourced jars to thousand&#45;hive blends, from strategically placed colonies along terrain that experiences similar weather and wind patterns, creating similar types of wildflower fields.</p>
+            <p>We recommend consuming anywhere between a teaspoon and a tablespoon of raw wildflower honey every day for about a month before noticing results. The amount necessary is dependent on each individual's body, and should really be played by ear. One can start small and increase, but maybe not experience early relief and must experiment with the timing of increased dosages. One can start big and wean off, and risk eating more than is necessary. There is, of course, a sweet spot, when it comes to honey, and luckily a <a href="recipes.html">plethora of ways to consume it</a>.</p> 
+        </section>
+    </article>
+    
+    <!-- PAGE FOOTER -->
+    <footer>
+        <ul>
+            <li><a href="honey.html">Honey</a></li>
+            <li><a href="markets.html">Markets</a></li>
+            <li><a href="recipes.html">Recipes</a></li>
+            <li><a href="about.html">About</a></li>
+        </ul>
+    </footer>
+<script>
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    var slideIndex = 1;
+    var autoPlay = setInterval(autoSlides, 5000);
+    
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+        clearInterval()
+    }
+    
+    function showSlides(n){
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        if (slideIndex < 1) {
+            slideIndex = slides.length;
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+    }
+    
+    function autoSlides(n) {
+        showSlides(0);
+        slideIndex++;
+    }
+    
+    /*function headerScroll() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementsByClassName("header").style.fontSize = "15pt";
+            document.getElementById("lsf-logo").style.width = "50%";
+        } else {
+             document.getElementsByClassName("header"(.style.fontSize = "17pt";
+        }
+    }
+    headerScroll();*/
+    autoSlides();
+</script>
+</body>
+</html>
